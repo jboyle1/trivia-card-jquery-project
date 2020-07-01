a2a75350baf905d5a753d8ca922739201e6463a8
Jed Boyle <jedboyle01@gmail.com>
2020-07-01 17:27:27 +0100

    create three different on 'click' event handlers. Each wrong answer has a different class. They are wrong-answer-one,wrong-answer-two, and wrong-answer-three. Leave all three callback functions empty for now.

--- static /js/main.js
+++ static /js/main.js
@@ -3,4 +3,16 @@ $(document).ready(() => {
     $('.hint-box').on('click', () => {
         $('.hint').slideToggle(800);
     });
+
+    $('wrong-answer-one').on('click', () => {
+
+    });
+
+    $('wrong-answer-two').on('click', () => {
+
+    });
+
+    $('wrong-answer-three').on('click', () => {
+
+    });
 });
